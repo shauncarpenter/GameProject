c04d586bb26d186487878a01bbcd53c3cc1907fe
Shaun Carpenter <shaun.carpenter88@gmail.com>
2013-08-12 22:15:49 +0800

    Now can extract vertex data into buffers

--- resource_management.c
+++ resource_management.c
@@ -35,3 +35,44 @@ aiScene* import_scene(char* file_name){
 void release_scene(aiScene* scene){
   aiReleaseImport(scene);
 }
+
+//Extracts a mesh from a given scene, storing it in a vertex buffer.
+Mesh extract_mesh(aiScene* scene, int mesh_num){
+  Mesh mesh;
+  aiMesh* assimp_mesh = scene->mMeshes[mesh_num];
+
+  //Create and bind the vertex buffer.
+  glGenBuffers(1, &(mesh.arrah_buffer));
+  glBindBuffer(GL_ARRAY_BUFFER, mesh.array_buffer);
+
+  //Place the vertex data in the buffer.
+  glBufferData(GL_ARRAY_BUFFER, sizeof(float)*assimp_mesh->mNumVertices*3,
+	       assimp_mesh->mVertices, GL_STATIC_DRAW);
+  
+  //Create and bind the index buffer.
+  glGenBuffers(1, &(mesh.element_buffer));
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh.element_buffer);
+  
+  //Transfer the indices from assimp format to a flat array.
+  unsigned int* indices = (unsigned int*)malloc(sizeof(unsigned int)*
+						assimp_mesh->mNumFaces*3);
+  for(int i=0; i<assimp_mesh->mNumFaces; i++){
+    aiFace* face = &assimp_mesh->mFaces[i];
+    memcpy(&indices[i*3], face->mIndices, 3*sizeof(unsigned int));
+  }
+
+  //Transfer that data to the buffer
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int)*mesh.num_faces*3,
+	       indices, GL_STATIC_DRAW);
+
+
+  //Later I will add extracting the texture and normal data.
+  mesh.num_faces = assimp_mesh->mNumFaces;
+  mesh.num_vertices = assimp_mesh->mNumVertices;
+
+  //Clean up
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+  free(indices);
+  return buffer;
+}
--- resource_management.h
+++ resource_management.h
@@ -1,4 +1,7 @@
 #include <assimp/scene.h>
 
+#include "mesh.h"
+
 aiScene* import_scene(char* file_name);
 void release_scene(aiScene* scene);
+Mesh extract_mesh(aiScene* scene, int mesh_num);
